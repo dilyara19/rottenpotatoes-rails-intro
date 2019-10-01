1812c624826216d429cd6e2f8d0b414f43d37d35
EC2 Default User <ec2-user@ip-172-31-21-81.us-east-2.compute.internal>
2019-10-01 22:04:39 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,7 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    
   end
   
   def index
@@ -15,10 +16,10 @@ class MoviesController < ApplicationController
     @movies = Movie.all 
     if params[:ratings].present?    
       @movies = Movie.where(rating: params[:ratings].keys)
-      session[:ratings] = params[:ratings].keys
+      session[:ratings] = params[:ratings]
     end
     if params[:sort]    
-      @movies = @movies.order(params[:sort]).where(rating: session[:ratings])
+      @movies = @movies.order(params[:sort]).where(rating: session[:ratings].keys)
         if params[:sort] == 'title'
           @title_header = 'hilite'
         elsif params[:sort] == 'release_date'
@@ -26,7 +27,8 @@ class MoviesController < ApplicationController
         end
       session[:current_sort] = params[:sort]
     end
-    session[:search_result] = params
+    current_uri = request.env['PATH_INFO']
+    flash[:last_search_path] = current_uri
   end
 
   def new
@@ -36,7 +38,8 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    redirect_to movies_path(:ratings => session[:ratings], :sort => session[:current_sort])
+    flash.keep
   end
 
   def edit
@@ -54,7 +57,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title} deleted."
-    redirect_to movies_path
+    redirect_to movies_path(:ratings => session[:ratings], :sort => session[:current_sort])
   end
 
 end
